dde7e29c345a9403394697bf5be5efadef8e8d31
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-01-27 17:39:58 +0900

    Dont auto-include CSS in npm build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import Form from './lib/form'
+import './style.css'
 
 module.exports = function attach (element, options) {
   render(<Form {...options} />, element)
